7bd9173db1e7e37ce07f79a1043a89335cf9df49
donaldodige <dodige@gmail.com>
2018-07-23 23:15:49 -0400

    add vt db

--- welcome/admin.py
+++ welcome/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from .models import PageView
+from .models import Info_membres
 
 # Register your models here.
 
@@ -9,3 +10,11 @@ class PageViewAdmin(admin.ModelAdmin):
     list_display = ['hostname', 'timestamp']
 
 admin.site.register(PageView, PageViewAdmin)
+#admin.site.register(PageView, PageViewAdmin)
+
+
+class Info_membresViewAdmin(admin.ModelAdmin):
+    list_display = ['numero', 'nom', 'prenom']
+
+
+admin.site.register(Info_membres, Info_membresViewAdmin)
